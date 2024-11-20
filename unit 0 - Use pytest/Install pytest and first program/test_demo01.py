a9b3af66725880fd8a5ffcfc057ad65ee245cbb3
Ngô Thị Kim Duyên <ttemp8399@gmail.com>
2024-11-20 11:43:16 +0700

    update file code

---
+++ unit 0 - Use pytest/Install pytest and first program/test_demo01.py
@@ -0,0 +1,12 @@
+def func(x):
+    return x + 1
+
+class TestCases:
+    def test_1(self):
+        assert func(2) == 3
+
+    def test_2(self):
+        assert func(3) == 5
+
+    def test_3(self):
+        assert func(4) == 5
